1b0c55f06f9bc26d6cd1028757c33e7a74f91571
Ben Cole <benjamin.cole@apple.com>
2024-08-01 16:25:03 -0400

    removed files from repo

--- src/middleware/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-export const authorize = (roles) => {
-    return (req, res, next) => {
-        if (!req.user || !roles.includes(req.user.roles[0])) {
-            return res.status(403).json({ message: 'Forbidden' });
-        }
-        next();
-    };
-};
